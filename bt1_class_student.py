cd8531123878063a81902c7e501e5a1cf86830fc
quangnd <quangnd@awing.vn>
2024-09-03 15:47:21 +0700

    fix: refactor bt1

--- bt1_class_student.py
+++ bt1_class_student.py
@@ -1,37 +1,37 @@
 class Student:
-    def __init__(self, student_id, student_name, math_grade, literature_grade, chemistry_grade):
+    def __init__(self, student_id: str, student_name: str, math_grade: int, literature_grade: int , chemistry_grade: int ) -> None:
         self.student_id = student_id
         self.student_name = student_name
         self.math_grade = math_grade
         self.literature_grade = literature_grade
         self.chemistry_grade = chemistry_grade
 
-    def average_grade(self):
+    def average_grade(self) -> int:
         return (self.math_grade + self.literature_grade + self.chemistry_grade) / 3
 
-    def __str__(self):
+    def __str__(self) -> str:
         return (f"Student id: {self.student_id}, Student name: {self.student_name}, Math grade: {self.math_grade}, Literature grade: {self.literature_grade}, Chemistry grade: {self.chemistry_grade}")
 
 class StudentManager:
-    def __init__(self):
+    def __init__(self) -> None:
         self.students = []
 
-    def add_student(self, student):
+    def add_student(self, student: Student) -> None:
         self.students.append(student)
 
-    def print_students_with_avg_grade_above(self, grade):
+    def print_students_with_avg_grade_above(self, grade: int) -> str:
         print(f"List students with average grade above {grade}:")
         for student in self.students:
             if student.average_grade() > grade:
                 print(f"{student}, Average grade: {student.average_grade():.1f}")
         print()
 
-    def print_students_with_chemistry_grade_below(self, grade):
+    def print_students_with_chemistry_grade_below(self, grade: int) -> str:
         print(f"List students with chemistry grade below {grade}:")
         for student in self.students:
             if student.chemistry_grade < grade:
                 print(student)
-    def print_all_students(self):
+    def print_all_students(self) -> str:
         print("List of all students:")
         for student in self.students:
             print(student)
